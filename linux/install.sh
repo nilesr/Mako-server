ac302c8cf15fb553e2a99a975a05289b4fecab47
Niles Rogoff <nilesrogoff@gmail.com>
2016-01-01 11:51:58 -0500

    Updated linux install script to use systemd

--- linux/install.sh
+++ linux/install.sh
@@ -7,25 +7,29 @@ echo How do you run your program? Include full paths, and no quotes
 echo "Example: '/etc/mako-server/server.py' or '/etc/mako-server/server.py -P /var/run/mako-server.pid'"
 read -p "> " arguments
 echo What user should run this program?
-echo "Example: '_www', 'www-data'"
+echo "Example: '_www', 'www-data', 'http'"
 echo "IMPORTANT! Make sure this user can modify the directory the server is in (or at least temporary_files), as well as the logfile"
 read -p "> " user
-echo script > /etc/init/mako-server.conf
-echo sudo -u $user $arguments >> /etc/init/mako-server.conf
-echo end script >> /etc/init/mako-server.conf
-echo start on startup >> /etc/init/mako-server.conf
+echo '[Unit]' > /etc/systemd/system/mako-server.service
+echo 'Description=Mako-server' >> /etc/systemd/system/mako-server.service
+echo '' >> /etc/systemd/system/mako-server.service
+echo '[Service]' >> /etc/systemd/system/mako-server.service
+echo ExecStart=/usr/bin/sudo -u $user $arguments >> /etc/systemd/system/mako-server.service
+echo '' >> /etc/systemd/system/mako-server.service
+echo '[Install]' >> /etc/systemd/system/mako-server.service
+echo 'WantedBy=multi-user.target' >> /etc/systemd/system/mako-server.service
 echo Installed. Start server now?
 echo -n "[y]> "
 read yn
 if test "$yn" == "" || test "$yn" == "y" || test "$yn" == "Y"; then
 	echo Starting
-	service mako-server start	
+	sudo systemctl start mako-server
 else
 	echo Aborting.
 fi
 echo This installer uses upstart. To control your server, use:
 echo
-echo sudo service mako-server '<'start'|'stop'|'restart'|'status'>'
+echo sudo systemctl '<'start'|'stop'|'restart'|'status'>' mako-server
 echo
 echo Installer exiting. mako-server configured to start on boot.
 echo The mako-server upstart process will not restart if it fails.
